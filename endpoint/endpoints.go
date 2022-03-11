270f1e5dbf9c65d27b77d0058cdf9eb6d0ff5abc
CoolSnow <coolsnow2020@gmail.com>
2022-03-11 16:56:13 +0800

    feat: micro

---
+++ endpoint/endpoints.go
@@ -0,0 +1,72 @@
+package endpoint
+
+import (
+	"context"
+	"micro-playgound/service"
+
+	"github.com/go-kit/kit/endpoint"
+)
+
+type DiscoveryEndpoints struct {
+	SayHelloEndpoint    endpoint.Endpoint
+	DiscoveryEndpoint   endpoint.Endpoint
+	HealthCheckEndpoint endpoint.Endpoint
+}
+
+// say hello
+type SayHelloRequest struct {
+}
+
+type SayHelloResponse struct {
+	Message string `json:"message"`
+}
+
+func MakeSayHelloEndpoint(svc service.Service) endpoint.Endpoint {
+	return func(ctx context.Context, request interface{}) (response interface{}, err error) {
+		message := svc.SayHello()
+		return SayHelloResponse{
+			Message: message,
+		}, nil
+	}
+}
+
+// discover service
+type DiscoveryRequest struct {
+	ServiceName string
+}
+
+type DiscoveryResponse struct {
+	Instances []interface{} `json:"instances"`
+	Error     string        `json:"error"`
+}
+
+func MakeDiscoveryEndpoint(svc service.Service) endpoint.Endpoint {
+	return func(ctx context.Context, request interface{}) (response interface{}, err error) {
+		req := request.(DiscoveryRequest)
+		instances, err := svc.DiscoveryService(ctx, req.ServiceName)
+		var errString = ""
+		if err != nil {
+			errString = err.Error()
+		}
+		return &DiscoveryResponse{
+			Instances: instances,
+			Error:     errString,
+		}, nil
+	}
+}
+
+// health check
+type HealthRequest struct{}
+
+type HealthResponse struct {
+	Status bool `json:"status"`
+}
+
+func MakeHealthCheckEndpoint(svc service.Service) endpoint.Endpoint {
+	return func(ctx context.Context, request interface{}) (response interface{}, err error) {
+		status := svc.HealthCheck()
+		return HealthResponse{
+			Status: status,
+		}, nil
+	}
+}
---
+++ go.mod
@@ -0,0 +1,10 @@
+module micro-playgound
+
+go 1.15
+
+require (
+	github.com/go-kit/kit v0.12.0
+	github.com/gorilla/mux v1.8.0
+	github.com/hashicorp/consul/api v1.12.0
+	github.com/satori/go.uuid v1.2.0 // indirect
+)
